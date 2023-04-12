377aacf1dbb22f348206cb29805e162becfb1997
Amras <amrasanor@naver.com>
2023-04-12 18:02:19 +0900

    app: signup

--- FishGrapher/Mureojo/config/urls.py
+++ FishGrapher/Mureojo/config/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('map.urls')),
-]
+    path('signup/', include('signup.urls')),
+    ]
 
