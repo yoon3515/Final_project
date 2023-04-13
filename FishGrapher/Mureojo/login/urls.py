1f850bfcbdd929d1a63baaba82dca94c233544c2
Amras <amrasanor@naver.com>
2023-04-13 16:45:07 +0900

    login

--- FishGrapher/Mureojo/login/urls.py
+++ FishGrapher/Mureojo/login/urls.py
@@ -1,11 +1,10 @@
 from django.urls import path
-
+from django.contrib.auth import views as auth_views
 from . import views
 
-app_name = "login"
+app_name = 'login'
 
 urlpatterns = [
-    path("", views.index, name="login"),
-    path("register/", views.index, name="register"),
-    path("find/", views.index, name="find"),
+    path('', auth_views.LoginView.as_view(template_name='login/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
