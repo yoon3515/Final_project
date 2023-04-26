545014ec6fae1c74807f0acdaf549d025e28376b
Amras <amrasanor@naver.com>
2023-04-26 16:42:14 +0900

    0426_1

--- FishGrapher/Mureojo/config/settings.py
+++ FishGrapher/Mureojo/config/settings.py
@@ -82,8 +82,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'log_db',
+        'HOST': '15.152.232.36',
+        'PORT': '3306',
+        'USER': 'rhkdgus',
+        'PASSWORD': 'Asdf!234',
     },
     'fish_db': {
         'ENGINE': 'django.db.backends.mysql',
@@ -95,8 +99,10 @@ DATABASES = {
     }
 }
 
-DATABASE_ROUTERS = ['config.routers.DefaultRouter', 'config.routers.FishRouter']
-AUTH_USER_MODEL = 'accounts.User'
+DATABASE_ROUTERS = ['config.routers.FishRouter']
+# AUTH_USER_MODEL = 'accounts.User'
+
+SITE_ID = 1
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
