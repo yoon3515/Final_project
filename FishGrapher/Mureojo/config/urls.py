f854a957fbd02258fee39b987d1e542550c2f388
yoon3515 <holly7574@gmail.com>
2023-04-28 21:41:09 +0900

    modified css style

--- FishGrapher/Mureojo/config/urls.py
+++ FishGrapher/Mureojo/config/urls.py
@@ -3,14 +3,12 @@ from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from map.views import get_fishing_spots
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='base.html'), name='home'),
     path('accounts/', include('accounts.urls'), name='accounts'),
     path('map/', include('map.urls')),
-    path('get_fishing_spots/', get_fishing_spots, name='get_fishing_spots'),
     path('fishBook/', include('fishBook.urls')),
     path('fish_info/', include('fish_info.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
