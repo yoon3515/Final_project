d1d62b2159feeee0fa3230d25876fb6e6707d239
yoon3515 <holly7574@gmail.com>
2023-04-28 11:27:34 +0900

    finis!

--- FishGrapher/Mureojo/fishBook/views.py
+++ FishGrapher/Mureojo/fishBook/views.py
@@ -20,38 +20,46 @@ def my_caught_fish_list(request):
     # 중복을 제거한 fish_name 가져오기
     fish_names = (
         CaughtFishInfo.objects.filter(member=user)
-        .values('fish_book__fish_name')
-        .annotate(count=Count('id'))
-        .order_by('fish_book__fish_name')
+        .values_list('fish_book__fish_name', flat=True)
         .distinct()
     )
 
     # FishBook 정보 가져오기
     fish_books = []
     for fish_name in fish_names:
-        fish_book = FishBook.objects.filter(fish_name=fish_name['fish_book__fish_name']).first()
+        fish_book = FishBook.objects.filter(fish_name=fish_name).first()
         if fish_book:
-            fish_book.count = fish_name['count']
+            count = CaughtFishInfo.objects.filter(member=user, fish_book__fish_name=fish_name).count()
+            fish_book.count = count
             fish_books.append(fish_book)
+
+    if not fish_books:  # fish_books가 빈 리스트인 경우
+        return render(request, 'fishBook/book.html', {'caught_fishes': caught_fishes})
 
     return render(request, 'fishBook/book.html', {'caught_fishes': caught_fishes, 'fish_books': fish_books})
 
 
 
+
+@login_required
 def search_fish(request):
     query = request.GET.get('search')
-    result_fishes = CaughtFishInfo.objects.none()
+    result_fishes = CaughtFishInfo.objects.filter(member=request.user, fish_book__fish_name__icontains=query)
     result_fish_books = []
     if query:
-        result_fishes = CaughtFishInfo.objects.filter(fish_book__fish_name__icontains=query)
+        result_fishes = result_fishes.filter(fish_book__fish_name__icontains=query)
         for fish in result_fishes:
             fish_book = FishBook.objects.filter(fish_name=fish.fish_book.fish_name).first()
             if fish_book:
                 result_fish_books.append(fish_book)
 
-    no_result = not result_fishes and query is not None
+    all_fishes = FishBook.objects.values_list('fish_name', flat=True)
 
-    return render(request, 'fishBook/book_result.html', {'query': query, 'result': result_fishes, 'result_fish_books': result_fish_books, 'no_result': no_result})
+    return render(request, 'fishBook/book_result.html', {'query': query,
+                                                         'result': result_fishes,
+                                                         'result_fish_books': result_fish_books,
+                                                         'all_fishes': all_fishes})
+
 
 
 # def search_fish(request):
--- FishGrapher/Mureojo/fish_info/views.py
+++ FishGrapher/Mureojo/fish_info/views.py
@@ -7,5 +7,7 @@ from django.contrib.auth.decorators import login_required
 
 def fish_info(request, fish_id):
     fish = get_object_or_404(FishBook, pk=fish_id)
-    caught_fish = MyCaughtFish.objects.filter(fish=fish_id)
-    return render(request, 'fish_info/fish_info.html', {'fish': fish, 'caught_fish': caught_fish})
+    caught_fish = MyCaughtFish.objects.filter(fish_book=fish_id, member=request.user)
+    total_caught = caught_fish.count()
+    return render(request, 'fish_info/fish_info.html',
+                  {'fish': fish, 'caught_fish': caught_fish, 'total_caught': total_caught})
