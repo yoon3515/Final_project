6a1ade8104f80d7507dfc1b0117a8ecf9a6bca73
Amras <amrasanor@naver.com>
2023-04-28 10:41:50 +0900

    0427_1

--- FishGrapher/Mureojo/config/urls.py
+++ FishGrapher/Mureojo/config/urls.py
@@ -4,11 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from map.views import get_fishing_spots
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='base.html'), name='home'),
+    path('', TemplateView.as_view(template_name='main.html'), name='home'),
     path('accounts/', include('accounts.urls'), name='accounts'),
     path('map/', include('map.urls')),
     path('get_fishing_spots/', get_fishing_spots, name='get_fishing_spots'),
+    path('pictures/', include('pictures.urls'), name='pictures'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
