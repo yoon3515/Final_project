a39cb1fb0fe6f6b2e8e345e9bbed2162bca7ee21
Amras <amrasanor@naver.com>
2023-04-28 14:34:45 +0900

    0428_1

--- FishGrapher/Mureojo/accounts/urls.py
+++ FishGrapher/Mureojo/accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
   path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
   path('signup/', views.signup, name='signup')
+#  path('find/', views.find, name='find')
 ]
--- FishGrapher/Mureojo/config/routers.py
+++ FishGrapher/Mureojo/config/routers.py
@@ -1,5 +1,5 @@
 class FishRouter:
-    route_app_labels = {'accounts','map'}
+    route_app_labels = {'map','pictures'}
     db_name = 'fish_db'
 
     def db_for_read(self, model, **hints):
