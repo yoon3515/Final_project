2ae451dca69f8a2f1a4ec67dab0a82cc06807f3f
Amras <amrasanor@naver.com>
2023-05-01 14:38:04 +0900

    0501_3

--- FishGrapher/Mureojo/accounts/urls.py
+++ FishGrapher/Mureojo/accounts/urls.py
@@ -7,6 +7,9 @@ app_name = 'accounts'
 urlpatterns = [
   path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
   path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-  path('signup/', views.signup, name='signup')
-#  path('find/', views.find, name='find')
+  path('signup/', views.signup, name='signup'),
+  path('password_reset/', auth_views.PasswordResetView.as_view(), name="password_reset"),
+  path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+  path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+  path('password_reset_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
