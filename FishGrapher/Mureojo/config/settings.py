0828a189e16d6b2976a533cd91ff19cd19995196
yoon3515 <holly7574@gmail.com>
2023-05-03 05:27:11 +0900

    v6 modified all css(book, info, base)

--- FishGrapher/Mureojo/config/settings.py
+++ FishGrapher/Mureojo/config/settings.py
@@ -144,6 +144,7 @@ STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
+    BASE_DIR / 'static/js',
 ]
 
 
--- FishGrapher/Mureojo/fish_info/views.py
+++ FishGrapher/Mureojo/fish_info/views.py
@@ -7,7 +7,9 @@ from django.contrib.auth.decorators import login_required
 
 def fish_info(request, fish_id):
     fish = get_object_or_404(FishBook, pk=fish_id)
+    # fish.image_url = fish.image.url
     caught_fish = MyCaughtFish.objects.filter(fish_book=fish_id, member=request.user)
     total_caught = caught_fish.count()
+    # caught_fish.image_url = caught_fish.my_photo.url
     return render(request, 'fish_info/fish_info.html',
                   {'fish': fish, 'caught_fish': caught_fish, 'total_caught': total_caught})
